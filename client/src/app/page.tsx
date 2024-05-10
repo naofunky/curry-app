454dd323b236fc42aa5d28efd675fe7571762753
Koki Nanao <minsokosoko0107@gmail.com>
2024-05-11 00:16:30 +0900

    init

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,20 +1,3 @@
 export default function Home() {
-  return (
-    <div className="card-container">
-      <div className="card">
-        <h2 className="card-title">Feeling Good</h2>
-        <img
-          className="image"
-          src="./favicon.ico"
-          width="360"
-          height="240"
-          alt=""
-        />
-        <p className="description">I AM A CAT. As yet I have no name.</p>
-        <div className="label">
-          <span className="material-symbols-outlined">pets</span>Goods
-        </div>
-      </div>
-    </div>
-  );
+  return <div>テスト</div>;
 }
